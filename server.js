b36eeab9eb14255fb1045ea679a5e9b2fa221081
Carson Yong <CarsonYong@gmail.com>
2015-08-13 14:33:17 -0400

    Added player and signup routes and turned them into ejs files. Also, fixed layout issue

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 var express = require('express');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+var ejs = require('ejs');
 
 
 
 var app = express();
-
+var expressLayouts = require('express-ejs-layouts')
+app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+// app.set('layout', { layout:'layout.ejs'});
+app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('index.ejs', {layout: false, 
-    title: "test title"},
+  response.render('index.ejs', {layout: 'layout'},
     function(err, html){
       response.send(html);
     });
-
 })
 
+app.get('/signup', function(request, response) {
+  response.render('signup.ejs', {layout: 'layout'},
+    function(err, html){
+      response.send(html);
+    });
+})
+
+app.get('/player', function(request, response) {
+  response.render('player.ejs', {layout: 'layout'},
+    function(err, html){
+      response.send(html);
+    });
+})
 
 app.listen(3000);
 console.log("Server is listening")
