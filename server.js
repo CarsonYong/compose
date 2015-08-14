032cb0ce3f30158d4a4b07dae1b7a98e22c8fe05
Lianne <lyharrison@sky.com>
2015-08-14 00:26:10 +0000

    added more styling to background tiles as well as the nav bar

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ var express = require('express');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var ejs = require('ejs');
-
-
-
 var app = express();
 var expressLayouts = require('express-ejs-layouts')
+
 app.set('view engine', 'ejs');
 app.set('layout', 'layout');
 // app.set('layout', { layout:'layout.ejs'});
+
 app.use(expressLayouts)
 app.use(express.static(__dirname + '/public'));
 
@@ -18,7 +17,7 @@ app.get('/', function(request, response) {
     function(err, html){
       response.send(html);
     });
-})
+});
 
 app.get('/signup', function(request, response) {
   response.render('signup.ejs', {layout: 'layout'},
