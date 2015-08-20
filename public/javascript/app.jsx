c7557cc46d04c7bbdcf00f44498f072f3aab370a
Lianne <lyharrison@sky.com>
2015-08-20 20:32:36 +0000

    started on homepage with HTML and css when in react

--- public/javascript/app.jsx
+++ public/javascript/app.jsx
@@ -4,9 +4,7 @@ var render = function() {
     document.getElementById('app')
   )
 }
-
 var arrayWords = [];
-
 var state = {
   songId: null,
   songName: null,
@@ -25,7 +23,6 @@ var state = {
     state.searchResults = results;
     render()
   },
-
   updateSong: function(result) {
     state.songId = result.id;
     state.songArtist = result.artists[0].name;
@@ -33,29 +30,24 @@ var state = {
     state.getLyrics();
     render()
   },
-
   updateImgURL: function(url){
     render()
   },
-
   getSearchResults: function() {
     $.ajax({
-      url: "https://api.spotify.com/v1/search?q="+state.searchQuery+"&type=artist,track,album&limit=3", 
+      url: "https://api.spotify.com/v1/search?q="+state.searchQuery+"&type=artist,track,album&limit=3",
       method:'get',
       dataType: 'json',
-    }).success(function(data){    
+    }).success(function(data){
       state.updateSearchResults({albums: data.albums.items, artists: data.artists.items, tracks:data.tracks.items });
     })
   },
-
   getLyrics: function() {
-
     var getImages = function(words){
-
       if(words.length){
         var word = words.pop();
         $.ajax({
-          url: "https://api.instagram.com/v1/tags/"+word+"/media/recent?client_id=",
+          url: "https://api.instagram.com/v1/tags/"+word+"/media/recent?client_id=aca67fb22e014089978d27ff7b7dc09b",
           method: "GET",
           dataType: 'jsonp'
         }).success(function(data){
@@ -63,19 +55,17 @@ var state = {
           for(var i = 0; i< arr.length; i ++) {
             var img = arr[i];
             var url = img.images.low_resolution.url;
-            state.instaArr.push(url) 
+            state.instaArr.push(url)
           }
           getImages(words)
         })
       } else {
           state.updateImgURL(state.instaArr)
-
       }
-      
-    }
 
+    }
     $.ajax({
-        url:"http://developer.echonest.com/api/v4/song/search?api_key=&artist="+state.songArtist+"&title="+state.songName+"&results=11&bucket=tracks&bucket=id:musixmatch-WW",
+        url:"http://developer.echonest.com/api/v4/song/search?api_key=IMAYEMDMYKLRNRLUT&artist="+state.songArtist+"&title="+state.songName+"&results=11&bucket=tracks&bucket=id:musixmatch-WW",
         method: 'get',
         dataType: 'json'
         }).success(function(data){
@@ -85,7 +75,7 @@ var state = {
           id = musixmatchId.split("song:")
           id = parseInt(id[1])
           $.ajax({
-            url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=&track_id="+id+"&format=jsonp",
+            url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=79872c3e3755258397333acc60d0244f&track_id="+id+"&format=jsonp",
             method: 'get',
             dataType: 'jsonp'
           }).success(function(data){
@@ -101,13 +91,7 @@ var state = {
               getImages(arrayWords)
           })
         })
-
       })// ends first success
-
 } // Ends getLyrics
-
 } // Ends variable state
-
-
-
 render()
--- public/javascript/templates.jsx
+++ public/javascript/templates.jsx
@@ -1,3 +1,23 @@
+var Navbar = React.createClass({
+  render: function(){
+    return (
+      <nav id="navbar-player" className="navbar">
+      <a id="navbar-brand" href="/">Compose</a>
+      <iframe src={"https://embed.spotify.com/?uri=spotify:track:"+state.songId} width="300" height="80" frameborder="0" allowtransparency="true"></iframe>
+       <ul>
+         <li><form id="search-player">
+           <label for="search-input-player" id="search-input-player-label"><img id="search-icon" src="/images/ic_search_white_48dp.png" alt="search image"></img></label>
+           <input type="text" name="search-input-player" className="arrow_box" id="search-input-player" placeholder="Search for more music"/>
+         </form></li>
+         <li>SIGN UP</li>
+         <li>LOGIN</li>
+         <li>LOGOUT</li>
+       </ul>
+      </nav>
+    )
+  }
+});
+
 var SearchResultView = React.createClass({
   pickSong: function() {
     state.updateSong(this.props.result)
@@ -7,7 +27,7 @@ var SearchResultView = React.createClass({
       <div onClick={this.pickSong}>{this.props.result.name}</div>
     )
   }
-})
+});
 
 var SearchView = React.createClass({
   updateQuery: function(event) {
@@ -32,22 +52,20 @@ var SearchView = React.createClass({
     return (
 
       <div>
-        <input type="text" placeholder="Search" onChange={this.updateQuery} value={state.searchQuery}/>
+        <input type="text"  name="search" id="search-input" value="" placeholder="Search Spotify for an artist, song or album" onChange={this.updateQuery} value={state.searchQuery}/>
         <button onClick={state.getSearchResults}>Search</button>
         {albumResultViews}
         {songResultViews}
         {artistResultViews}
       </div>
-      )
+    )
   }
 
-})
+});
 
 
 var PlayerView = React.createClass({
   render: function() {
-    
-      
     var arr = []
     var imageArray = state.instaArr
 
@@ -55,42 +73,48 @@ var PlayerView = React.createClass({
       arr.push(<img src={imageArray[i]}/>)
     }
 
-      return  (
-
-      <div>
+      return (
         <div>
-          <iframe src={"https://embed.spotify.com/?uri=spotify:track:"+state.songId} width="300" height="80" frameborder="0" allowtransparency="true"></iframe>
+          {Navbar}
+          <div className="player-wrapper">
+            <div className="player-floated-img">
+              {arr}
+            </div>
+          </div>
         </div>
-
-     
-          <div>
-            {arr}
-           </div> 
-        
-          
-      </div>
       )
-
   }
 });
 
 var HomeView = React.createClass({
   render: function () {
-    var greeting = ""
-
-    if(state.searchQuery) {
-      greeting = "Hello "+state.searchQuery+"!"
-    } else {
-      greeting = "Hello Nobody. :("
-    }
 
     return (
       <div>
         <header id="header">
-          {greeting}
+          <Navbar/>
         </header>
 
-        <SearchView />  
+        <div className="background-wrapper">
+
+          <div className="page-content">
+
+            <div className="landing-page">
+
+              <h1>Visualise your favourite songs with Compose</h1>
+
+              <div id="search-form">
+                <form>
+                  <SearchView />
+                </form>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
       </div>
       )
   }
