08404a6b0d91d6fca6c28fadfc68ce881cbcb619
Carson Yong <CarsonYong@gmail.com>
2015-08-21 13:41:46 -0400

    Fixed getting song name and artist when clicking on the song from search results, used encodeURIComponent

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(".floated-img").empty();
   $.ajax({
-    url: "https://api.instagram.com/v1/tags/lovemusic/media/recent?client_id=6b57da6cc49c4e2ca5af262214decb93",
+    url: "https://api.instagram.com/v1/tags/lovemusic/media/recent?client_id=",
     method: "GET",
     dataType: 'jsonp'
   }).success(function(data){
@@ -22,7 +22,7 @@ $(document).ready(function() {
     // alert(searchQuery);
 
    $.ajax({
-    url: "https://api.spotify.com/v1/search?q="+searchQuery+"&type=artist,track,album&limit=3", 
+    url: "https://api.spotify.com/v1/search?q="+searchQuery+"&type=artist,track,album&limit=1", 
      method:'get',
      dataType: 'json',
      error: function (xhr, ajaxOptions, thrownError) {
@@ -30,6 +30,7 @@ $(document).ready(function() {
       },
      success: function(html){
       var data = (html)
+      console.log(data)
       var albums = (data.albums.items)
       for(var i = 0; i < albums.length; i++) {
         var album = albums[i];
@@ -52,7 +53,11 @@ $(document).ready(function() {
         var songId = song.id;
         var songName = song.name;
         var songArtist = song.artists[0].name;
-        $("<li id="+songId+" songName="+songName+" songArtist="+songArtist+">"+songName+"</li>").appendTo("#songs");
+        $("<li/>", {
+          id:songId,
+          songname:songName,
+          songartist:songArtist,
+          text:songName}).appendTo("#songs");
         $("<li id="+songId+" songName="+songName+" songArtist="+songArtist+">"+songArtist+"</li>").appendTo("#songs");
          //console.log("Song ID: " + songId + " -- Song Name: "+ songName+ " -- Artist: "+ songArtist)
 
@@ -63,8 +68,8 @@ $(document).ready(function() {
 
         var songId = ($(this).attr('id'));
         console.log(songId)
-        var songName = $(this).attr('songName');
-        var songArtist = $(this).attr('songArtist');
+        var songName = encodeURIComponent($(this).attr('songName'));
+        var songArtist = encodeURIComponent($(this).attr('songArtist'));
         console.log(songId)
         console.log(songName)
         console.log(songArtist)
@@ -74,7 +79,7 @@ $(document).ready(function() {
       $('<iframe src="https://embed.spotify.com/?uri=spotify:track:'+songId+'" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>').appendTo(".navbar");
       //Make ajax call for musixmatch song id number
       $.ajax({
-        url:"http://developer.echonest.com/api/v4/song/search?api_key=FZBHWASTWJKMBT0CU&artist="+songArtist+"&title="+songName+"&results=11&bucket=tracks&bucket=id:musixmatch-WW",
+        url:"http://developer.echonest.com/api/v4/song/search?api_key=&artist="+songArtist+"&title="+songName+"&results=11&bucket=tracks&bucket=id:musixmatch-WW",
         method: 'get',
         dataType: 'json'
         }).success(function(html){
@@ -87,10 +92,11 @@ $(document).ready(function() {
           console.log(id)
           // Start Lyrics
           $.ajax({
-            url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=c1652e120f3e1c24a918c09c65b219a9&track_id="+id+"&format=jsonp",
+            url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=&track_id="+id+"&format=jsonp",
             method: 'get',
             dataType: 'jsonp'
           }).success(function(html){
+            console.log(html)
             var data = (html);
             var lyrics = data.message.body.lyrics.lyrics_body;
             console.log(lyrics)
@@ -104,7 +110,7 @@ $(document).ready(function() {
               for(var i = 0; i < data.length; i ++) {
                 var word = data[i];
                 $.ajax({
-                  url: "https://api.instagram.com/v1/tags/"+data[i]+"/media/recent?client_id=6b57da6cc49c4e2ca5af262214decb93",
+                  url: "https://api.instagram.com/v1/tags/"+data[i]+"/media/recent?client_id=",
                   method: "GET",
                   dataType: 'jsonp'
                 }).success(function(data){
